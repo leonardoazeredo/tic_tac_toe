e401652abd420d37a07f83a4bc5f635183fc326a
Leonardo Azeredo <leonardo.azeredo@live.com>
2018-04-18 12:19:14 -0300

    added check for row win

--- board.rb
+++ board.rb
@@ -5,6 +5,18 @@ class Board
     @grid = Array.new(3) { Array.new(3, :" ") }
   end
 
+  def row_win?
+    @grid.any? do |row|
+      row.all? do |cell|
+        cell == :X
+      end
+    end
+  end
+
+  def collumn_win?; end
+
+  def digonal_win?; end
+
   def print_grid
     @grid.each do |row|
       puts
@@ -15,5 +27,16 @@ class Board
     puts
   end
 
-  Board.new.print_grid
+  b = Board.new
+  b.print_grid
+  b.grid[0][0] = :X
+  b.print_grid
+  p b.row_win?
+  b.grid[0][1] = :X
+  b.print_grid
+  p b.row_win?
+  b.grid[0][2] = :X
+  b.print_grid
+  p b.row_win?
+
 end
